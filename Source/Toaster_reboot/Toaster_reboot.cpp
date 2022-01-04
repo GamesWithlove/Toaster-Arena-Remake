3d1532d793c465c4c5aae16e50082163a64f2df5
Shadow_hawk <juancoc84@gmail.com>
2022-01-04 18:52:29 -0400

    commit

---
+++ Source/Toaster_reboot/Toaster_reboot.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Toaster_reboot.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Toaster_reboot, "Toaster_reboot" );
