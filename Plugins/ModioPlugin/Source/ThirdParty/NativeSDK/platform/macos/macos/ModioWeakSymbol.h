21b8b31e5122edcec060c029ee2e1be8c7715acc
Shadow_hawk <juancoc84@gmail.com>
2023-08-27 01:15:09 -0400

    chunk 5

--- Plugins/ModioPlugin/Source/ThirdParty/NativeSDK/platform/macos/macos/ModioWeakSymbol.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#define MODIO_WEAK_STUB_IMPL {}
-
-#define MODIO_WEAK(Name) __attribute__((weak))
--- Plugins/SimpleUGC/Source/SimpleUGC/Private/ReplacementActorComponent.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "ReplacementActorComponent.h"
-
-// Sets default values for this component's properties
-UReplacementActorComponent::UReplacementActorComponent()
-{
-	UActorComponent::SetAutoActivate(true);
-}
