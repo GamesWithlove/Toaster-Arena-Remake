75e60c7ab1179f61df4ecdabe00821dacd562ae5
unselles <pumape2@seznam.cz>
2024-10-16 15:31:28 +0200

    Reworked loadout selection, various other widget changes

--- Source/Toaster_reboot/ToasterCustomizationPlayerInfoData.cpp
+++
@@ -1,1 +0,0 @@
-﻿#include "ToasterCustomizationPlayerInfoData.h"
---
+++ Source/Toaster_reboot/ToasterPlayerProfileData.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataTable.h"
+#include "Engine/Texture2D.h"
+#include "ToasterPlayerProfileData.generated.h"
+
+USTRUCT(BlueprintType)
+	struct TOASTER_REBOOT_API FToasterPlayerProfileData : public FTableRowBase
+	{
+	public:
+		GENERATED_USTRUCT_BODY()
+		
+		UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Customization Data")
+		TSoftObjectPtr<UTexture2D> PlayerBadge;
+	
+		UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Customization Data")
+		FName PlayerTag;
+
+		UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Customization Data")
+		bool IsLocked;
+	};
