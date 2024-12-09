85c4d7cc967dc3a7221d606329f01f96746a949c
unselles <pumape2@seznam.cz>
2024-12-10 00:40:52 +0100

    Update Toaster_reboot.Build.cs

--- Source/Toaster_reboot/Toaster_reboot.Build.cs
+++ Source/Toaster_reboot/Toaster_reboot.Build.cs
@@ -8,7 +8,7 @@ public class Toaster_reboot : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "AIModule", "UMG" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "AIModule", "UMG", "CommonUI", });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
