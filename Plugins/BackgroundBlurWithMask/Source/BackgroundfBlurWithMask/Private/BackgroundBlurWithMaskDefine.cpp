2792302a6f84fad1061ac5e003ad938e0a12484f
Shadow_hawk <juancoc84@gmail.com>
2024-12-09 20:00:34 -0400

    Removed plugins

--- Plugins/BackgroundBlurWithMask/Source/BackgroundfBlurWithMask/Private/BackgroundBlurWithMaskDefine.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Qibo Pang 2022. All Rights Reserved.
-
-#include "BackgroundBlurWithMaskDefine.h"
-#include "Styling/SlateBrush.h"
-#include "Materials/MaterialInterface.h"
-
--- Plugins/Toaster_mod_plugin/Source/ToasterGame/Private/HitscanImpactDefinition.cpp
+++
@@ -1,1 +0,0 @@
-#include "HitscanImpactDefinition.h"
