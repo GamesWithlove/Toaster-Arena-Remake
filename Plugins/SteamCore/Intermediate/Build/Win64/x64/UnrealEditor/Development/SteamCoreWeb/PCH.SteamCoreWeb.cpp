622737e7e2eae3b2e6363f0100815e37a15b76fc
Shadow_hawk <juancoc84@gmail.com>
2026-01-11 00:10:56 -0400

    Compilation work and bug fixes for source engine build

--- Plugins/SteamCore/Intermediate/Build/Win64/x64/UnrealEditor/Development/SteamCoreWeb/PCH.SteamCoreWeb.cpp
+++ Plugins/SteamCore/Intermediate/Build/Win64/x64/UnrealEditor/Development/SteamCoreWeb/PCH.SteamCoreWeb.cpp
@@ -1,2 +1,2 @@
-// Compiler: 14.29.30158
+// Compiler: 14.34.31948
 #include "E:/Ue4_projects/Toaster-Arena-Remake/Plugins/SteamCore/Intermediate/Build/Win64/x64/UnrealEditor/Development/SteamCoreWeb/PCH.SteamCoreWeb.h"
