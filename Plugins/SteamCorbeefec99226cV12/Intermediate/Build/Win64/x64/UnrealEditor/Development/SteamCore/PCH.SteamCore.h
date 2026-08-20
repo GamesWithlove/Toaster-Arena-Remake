ae442377d03433b738ceb26fe6c039b2116d6c36
BRVVVH <109293220+BRVVVH@users.noreply.github.com>
2026-08-21 01:35:52 +0300

    untracked files that are in .gitignore

--- Plugins/SteamCorbeefec99226cV12/Intermediate/Build/Win64/x64/UnrealEditor/Development/SteamCore/PCH.SteamCore.h
+++
@@ -1,6 +0,0 @@
-// PCH for SteamCorePluginPrivatePCH.h
-#include "Definitions.h"
-#include "SteamCorePluginPrivatePCH.h"
-#ifdef __ISPC_ALIGN__
-#error ispc.generated.h files are not allowed in precompiled headers (This is a build time optimization to reduce action dependencies)
-#endif
